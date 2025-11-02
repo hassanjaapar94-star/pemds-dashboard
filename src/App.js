d0a49ef150603c77e0f3c2ca79dc8bd4bd9117d9
hassanjaapar94-star <hassanjaapar94@gmail.com>
2025-11-02 13:56:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function App() {
   return (
-    <div>
-      <h1>MENRE Dashboard Works!</h1>
-      <p>If you see this, the React app is working.</p>
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h1 style={{ color: "#166534" }}>MENRE Tawi-Tawi Dashboard</h1>
+      <p>If you see this text, React is rendering correctly!</p>
     </div>
   );
 }
