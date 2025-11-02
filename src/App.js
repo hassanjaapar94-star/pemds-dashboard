7c8aa860e80e850e3be97dfa779a6c7137cf2318
hassanjaapar94-star <hassanjaapar94@gmail.com>
2025-11-02 13:36:00 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function App() {
-  return <h1>MENRE Dashboard Works!</h1>;
+  return (
+    <div>
+      <h1>MENRE Dashboard Works!</h1>
+      <p>If you see this, the React app is working.</p>
+    </div>
+  );
 }
