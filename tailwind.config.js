25dce175d1f3966bd0db62d57dc9189dc580f4ff
hassanjaapar94-star <hassanjaapar94@gmail.com>
2025-11-02 11:07:55 +0800

    Add Tailwind CSS configuration with custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        menreGreen: "#166534",
+        menreLight: "#bbf7d0"
+      }
+    }
+  },
+  plugins: []
+};
