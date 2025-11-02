5b42ea723db31a116f295b63d838effffad47bb7
hassanjaapar94-star <hassanjaapar94@gmail.com>
2025-11-02 13:10:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
 
 export default function App() {
-  return (
-    <div>
-      <h1>MENRE Tawi-Tawi Dashboard</h1>
-      <p>If you see this, the app is working.</p>
-    </div>
-  );
+  return <h1>MENRE Dashboard Works!</h1>;
 }
